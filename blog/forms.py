d1ec05aea0088525cf178aa639242171a04ac054
Vishal Narula <vishalnarula.iitd@gmail.com>
2023-12-29 18:42:11 +0000

    forms but to check comments bit

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django.forms import ModelForm
+from blog.models import Comment
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
+
